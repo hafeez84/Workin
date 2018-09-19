429f398278b1fdd595b72e3899e4ef7d7f3cd901
Hafizullah Qasemi <h.qasemy@live.com>
2018-09-19 17:02:04 +0000

    modified sendgrid confirmation mail sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.delivery_method = :test
+
+config.action_mailer.default_url_options = { :host => 'https://ruby-on-rails-hafeezullahqasemy.c9users.io/'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,11 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
+  
+  
+  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.default_url_options = { :host => 'hafeez-finance-tracker-app.herokuapp.com/', :protocol => 'https'}
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
